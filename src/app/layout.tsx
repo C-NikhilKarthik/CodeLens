373081b99e4bebe1580907e0d7ae2703d21a6ad1
Nikhil Karthik <nikhilkarthik241103@gmail.com>
2024-06-29 00:19:06 +0530

    feat: dashboard page added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Inter, Lexend } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });

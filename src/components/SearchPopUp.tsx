304c408b3d8dba3f48f5127bec8efb07542dde21
Mandy767 <92856810+Mandy767@users.noreply.github.com>
2024-06-29 13:54:24 +0530

    fix

--- src/components/SearchPopUp.tsx
+++ src/components/SearchPopUp.tsx
@@ -34,7 +34,7 @@ const Modal = ({ isOpen, onClose, children }: ComponentProps<any>) => {
         <div onClick={handleBackgroundClick} className={`fixed z-10 inset-0 bg-black bg-opacity-50 flex items-center justify-center transition-opacity ${isOpen ? 'opacity-100  ease-in-out duration-1000' : 'opacity-0 pointer-events-none  ease-in-out duration-1000'
             }`}>
             <div style={{ minWidth: '500px', minHeight: '200px' }}
-                className={`bg-white p-8 rounded-lg transform transition-transform ${isOpen ? 'scale-100  ease-in-out duration-1000' : 'scale-90  ease-in-out duration-1000'
+                className={`bg-white p-3 rounded-lg transform transition-transform ${isOpen ? 'scale-100  ease-in-out duration-1000' : 'scale-90  ease-in-out duration-1000'
                     }`}
             >
                 {children}
